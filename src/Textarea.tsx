a4be5f248f1a54f3482db606669259d44e8b105f
Kevin Gilbert <kevin@kegisiteweb.com>
2021-10-27 18:09:36 -0400

    Added Textarea control

---
+++ src/Textarea.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { TextareaProps, useControlProps } from '@concrete-form/core'
+
+import Control from './layout/Control'
+
+const Textarea: React.FC<TextareaProps> = ({
+  name,
+  ...inputProps
+}) => {
+  const props = useControlProps(name, inputProps)
+
+  return (
+    <Control name={name}>
+      <textarea
+        {...props}
+      />
+    </Control>
+  )
+}
+
+export default Textarea
