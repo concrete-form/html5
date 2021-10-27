5613eff40d139d28bf105958ee8f383da954ee8e
Kevin Gilbert <kevin@kegisiteweb.com>
2021-10-27 18:08:31 -0400

    Added AutoComplete control

---
+++ src/Autocomplete.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AutocompleteProps } from '@concrete-form/core'
+
+import Input from './Input'
+
+const Autocomplete: React.FC<AutocompleteProps> = ({
+  ...inputProps
+}) => <Input autoComplete="on" {...inputProps} type="text" />
+
+export default Autocomplete
